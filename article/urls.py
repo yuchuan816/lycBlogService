eaf15d71f5d73092348a3a5d7dbd9b1c7798d24b
liuyuchuan <liuyuchuan816@163.com>
2019-06-21 18:32:14 +0800

    m

--- article/urls.py
+++ article/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import ArticleView
+from rest_framework.routers import DefaultRouter
+from .views import ArticleViewSet
 
 
-urlpatterns = [
-    path('articles/', ArticleView.as_view()),
-]
+router = DefaultRouter()
+router.register(r'articles', ArticleViewSet, base_name='article')
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path('articles/', ArticleViewSet),
+# ]
--- article/views.py
+++ article/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status, generics
+from rest_framework import status, viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import ArticleSerializer
@@ -7,14 +7,14 @@ from .models import Article
 # Create your views here.
 
 
-@api_view(['GET', 'POST'])
-def get_article_list(request):
-    req = request.data
-    pk = req.get('id', None)
-    has_value = pk is not None
-    articles = Article.objects.get(pk=pk) if has_value else Article.objects.all()
-    serializer = ArticleSerializer(instance=articles, many=not has_value)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+# @api_view(['GET', 'POST'])
+# def get_article_list(request):
+#     req = request.data
+#     pk = req.get('id', None)
+#     has_value = pk is not None
+#     articles = Article.objects.get(pk=pk) if has_value else Article.objects.all()
+#     serializer = ArticleSerializer(instance=articles, many=not has_value)
+#     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 # @api_view(['GET', 'POST'])
@@ -25,16 +25,6 @@ def get_article_list(request):
 #         return Response(serializer.data, status=status.HTTP_200_OK)
 #     return Response({"error": serializer.errors}, status=status.HTTP_200_OK)
 
-
-# @api_view(['GET', 'POST'])
-# def modify_article():
-#     article = Article.objects.all()
-
-
-# @api_view(['GET', 'POST'])
-# def delete_article():
-#     article = Article.objects.all()
-
 # class ArticleView(APIView):
 #     def get(self, request, *args, **kwargs):
 #         articles = Article.objects.all()
@@ -42,6 +32,6 @@ def get_article_list(request):
 #         return Response(serializer.data)
 
 
-class ArticleView(generics.ListCreateAPIView):
-    queryset = Article.objects.all()
+class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
